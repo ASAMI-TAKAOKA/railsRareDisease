8a4e02dc34e1438654c12a0d3062dad529f06ec7
Asami Takaoka <asamiyamabe61@gmail.com>
2022-09-30 23:36:11 +0900

    scaffoldによりPOSTのCRUD実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_142247) do
+ActiveRecord::Schema.define(version: 2022_09_30_143437) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "titile"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
