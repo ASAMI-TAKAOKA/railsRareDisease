122d8ea04d7e7f4903d6c9cba0d9fe3ba333df92
Asami Takaoka <asamiyamabe61@gmail.com>
2022-10-05 22:31:58 +0900

    会員登録、ログイン時のバグ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_parameters, if: :devise_controller?
+
+  protected
+    def configure_sign_up_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get '/sign_in', to: 'users/sessions#new'
+  end
   resources :comments
   resources :posts
   resources :users
-  root to: "post#index"
+  root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
