87756e562f314d64e34e52099d1c842c8005a257
Asami Takaoka <asamiyamabe61@gmail.com>
2022-10-06 18:41:11 +0900

    投稿時と会員登録時のリダイレクト先をroot_pathに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_url, notice: "新規記事を投稿しました。" }
+        format.html { redirect_to root_path, notice: "新規記事を投稿しました。" }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
