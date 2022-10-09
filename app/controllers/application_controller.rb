47ad9451255e8ceb304ab9c153ef2265734789b8
Asami Takaoka <asamiyamabe61@gmail.com>
2022-10-09 16:53:07 +0900

    会員登録時のバグ修正、サインアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     def configure_sign_up_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     end
+
+    def after_sign_out_path_for(resource)
+      root_path
+    end
 end
