04a702981dd93d84542ae64520e346a7b34619ed
Asami Takaoka <asamiyamabe61@gmail.com>
2022-10-09 19:52:58 +0900

    headerの追加（要修正：ログインしているのにログイン後の画面が表示されない）

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   # アソシエーション
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  # has_secure_password
   # validates :name, presence: true
   # validates :gender, presence: true
   # validates :email, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get '/sign_in', to: 'users/sessions#new'
+    get '/users/sign_in', to: 'devise/sessions#new'
   end
   resources :comments
   resources :posts
